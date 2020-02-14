6d58e6d5cfb42662e02239146aa1ba34265c0d08
tom <tom@4x4.czest.pl>
2020-02-14 14:44:44 +0100

    testujemy czy przejdzie

--- plik1.py
+++ plik1.py
@@ -8,9 +8,32 @@ sqlparse   0.3.0
 python3 -m pip install --upgrade pip
 pip install -r requirements.txt
 
-----------
+----------https://tutorial.djangogirls.org/pl/django_start_project/
+https://docs.djangoproject.com/en/2.0/ref/models/fields/#field-types
+
 1---
 django-admin startproject mysite .
 
 Kropka . ma kluczowe znaczenie, ponieważ dzięki niej skrypt wie, że ma zainstalować
 Django w bieżącym katalogu (kropka . to taka skrócona nazwa bieżącego katalogu)
+
+#
+#Aby utworzyć bazę danych dla naszego bloga, wykonajmy następujące polecenie w konsoli:
+#(musimy być w katalogu djangogirls, tam gdzie znajduje się plik manage.py).
+python manage.py migrate
+
+#
+
+python manage.py runserver 127.0.0.1:8080
+
+#start aplikacji
+python manage.py startapp blog
+
+#Tworzymy tabele dla modeli w bazie danych
+python manage.py makemigrations blog
+python manage.py migrate blog
+
+#admini konto
+python manage.py createsuperuser
+#panel -- https://docs.djangoproject.com/en/2.0/ref/contrib/admin/
+
