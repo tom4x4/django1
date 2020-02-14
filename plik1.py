102234c1adb4066b627fcf13d320b7475c684a4e
tom <tom@4x4.czest.pl>
2020-02-14 12:43:51 +0100

    testujemy czy przejdzie

--- plik1.py
+++ plik1.py
@@ -4,4 +4,13 @@ pip        19.2.3
 pytz       2019.2
 setuptools 41.2.0
 sqlparse   0.3.0
-dsdsdd
+
+python3 -m pip install --upgrade pip
+pip install -r requirements.txt
+
+----------
+1---
+django-admin startproject mysite .
+
+Kropka . ma kluczowe znaczenie, ponieważ dzięki niej skrypt wie, że ma zainstalować
+Django w bieżącym katalogu (kropka . to taka skrócona nazwa bieżącego katalogu)
